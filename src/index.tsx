6b5c13a6480f7e394f93bb5ce90e66a9ed8636d5
antoninWD <anto94@live.ca>
2022-01-16 08:49:14 -0500

    redirecturi bug

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,8 @@ ReactDOM.render(
   <Auth0Provider
     domain={`${process.env.REACT_APP_AUTH_DOMAIN}`}
     clientId={`${process.env.REACT_APP_AUTH_CLIENT_ID}`}
-    redirectUri={`${window.location.href.split("/app")[0]}/app/home`}>
+    // https://webriken.netlify.app/
+    redirectUri={`${window.location.href.split("/app")[0]}`}>
     <NotificationsContextProvider>
       <HomeContextProvider>
         <ThemeContextProvider>
